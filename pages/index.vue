5bab6eb3a8d9df5ae2f9e3c393b6b80d3c3452c7
cnkbekir <cnkbekir@gmail.com>
2023-10-10 07:49:03 +0300

    add ionic

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-button @click="openModal">Open Modal</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+<script setup>
+  
+</script>
